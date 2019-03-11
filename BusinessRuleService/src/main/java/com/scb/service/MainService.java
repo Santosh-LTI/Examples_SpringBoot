eeb32553e01651081752d0108e27fea565498273
Santosh KUmar <44458930+Santosh-LTI@users.noreply.github.com>
2019-03-11 11:37:17 +0530

    Add files via upload

---
+++ BusinessRuleService/src/main/java/com/scb/service/MainService.java
@@ -0,0 +1,27 @@
+package com.scb.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.scb.model.BusinessRules;
+import com.scb.model.RequestData;
+import com.scb.model.ResponseMessage;
+
+@Service
+public interface MainService {
+	boolean saveBussinessRule(BusinessRules businessRules);
+
+	List<BusinessRules> getAllBusinessRules();
+
+	BusinessRules getBusinessRule(String ruleCode);
+
+	List <BusinessRules> getBusinessRulesByTypeandCountryCode(String transactionType, String transactionSubType);
+	
+	void ModifyBusinessRules(BusinessRules businessrules);
+
+	ResponseMessage validateBusinessRules(RequestData requestData);
+	
+	
+	void DeleteBusinessRule(String ruleCode);
+}
